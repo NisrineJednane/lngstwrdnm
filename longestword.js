1e5429712b40db94fe418e9db61d4e95dfefe0fc
NisrineJednane <34342007+NisrineJednane@users.noreply.github.com>
2019-10-17 13:16:51 +0100

    Update longestword.js

--- longestword.js
+++ longestword.js
@@ -1,6 +1,6 @@
 function longestWordLength(str) {
 
-//splits input string into invidivual words + creates array with length of each word
+//splits input string into individual words + creates array with length of each word
     let longestWordCount = str.split(" ").map(mapCount=>mapCount.length); 
     
 //return count of longest word
